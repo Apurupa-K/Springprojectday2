97029086099d013a4732ca42af5358c3823b190f
apurupa <apurupa@LAPTOP-SL8NQJ86>
2021-04-21 12:05:42 +0530

    Wild cards

---
+++ SpringProjectday2/src/aop/model/Triangle.java
@@ -0,0 +1,18 @@
+
+package aop.model;
+public class Triangle {
+	String name;
+
+	public Triangle() {}
+	public Triangle() {
+		System.out.println("in triangle constructor");}
+
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
